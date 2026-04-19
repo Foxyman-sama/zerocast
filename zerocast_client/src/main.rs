03bdf4f75198b604851f57da65c61fb1a6dcb3b8
Dmytro Latokho <dimalatoho1448@gmail.com>
2026-04-19 16:00:17 +0300

    feat: realize basic sending/receiving video packages

--- zerocast_client/src/main.rs
+++ zerocast_client/src/main.rs
@@ -1,6 +1,66 @@
-use zerocast_core::test_shared_logic;
+use gstreamer::prelude::*;
+use gstreamer::{Caps, Element, ElementFactory, MessageView, Pipeline, State};
 
 fn main() {
   println!("Starting ZeroCast Client...");
-  test_shared_logic();
+
+  gstreamer::init().expect("Failed to initialize GStreamer!");
+
+  let source = ElementFactory::make("udpsrc")
+    .name("udp_input")
+    .property("port", 5000i32)
+    .build()
+    .expect("Failed to create udpsrc");
+
+  let caps = Caps::builder("application/x-rtp")
+    .field("media", "video")
+    .field("clock-rate", 90000i32)
+    .field("encoding-name", "H264")
+    .build();
+  source.set_property("caps", &caps);
+
+  let depayloader = ElementFactory::make("rtph264depay")
+    .name("rtp_depayloader")
+    .build()
+    .expect("Failed to create rtph264depay");
+
+  let decoder = ElementFactory::make("avdec_h264")
+    .name("h264_decoder")
+    .build()
+    .expect("Failed to create avdec_h264");
+
+  let videoconvert = ElementFactory::make("videoconvert")
+    .build()
+    .expect("Failed to create videoconvert");
+
+  let sink = ElementFactory::make("autovideosink")
+    .build()
+    .expect("Failed to create autovideosink");
+
+  let pipeline = Pipeline::with_name("zerocast-receive-pipeline");
+  pipeline
+    .add_many([&source, &depayloader, &decoder, &videoconvert, &sink])
+    .unwrap();
+  Element::link_many([&source, &depayloader, &decoder, &videoconvert, &sink])
+    .unwrap();
+
+  println!("Listening for stream on UDP 5000... Waiting for server!");
+  pipeline.set_state(State::Playing).unwrap();
+
+  let bus = pipeline.bus().unwrap();
+  for msg in bus.iter_timed(gstreamer::ClockTime::NONE) {
+    match msg.view() {
+      MessageView::Error(err) => {
+        eprintln!("Error: {} ({:?})", err.error(), err.debug());
+        break;
+      }
+      MessageView::Eos(..) => {
+        println!("Stream ended.");
+        break;
+      }
+      _ => (),
+    }
+  }
+
+  pipeline.set_state(State::Null).unwrap();
 }
--- zerocast_server/src/main.rs
+++ zerocast_server/src/main.rs
@@ -1,3 +1,4 @@
+// Import
 use gstreamer::prelude::*;
 use gstreamer::{Element, ElementFactory, MessageView, Pipeline, State};
 use zerocast_core::test_shared_logic;
@@ -8,27 +9,46 @@ fn main() {
 
   gstreamer::init().expect("Failed to initialize GStreamer!");
 
-  let source = ElementFactory::make("gdiscreencapsrc")
+  let source = ElementFactory::make("d3d11screencapturesrc")
     .name("screen_source")
     .build()
-    .expect("Failed to create gdiscreencapsrc");
+    .expect("Failed to create d3d11screencapturesrc");
 
   let videoconvert = ElementFactory::make("videoconvert")
     .name("converter")
     .build()
     .expect("Failed to create videoconvert");
 
-  let sink = ElementFactory::make("autovideosink")
-    .name("video_output")
+  let encoder = ElementFactory::make("x264enc")
+    .name("h264_encoder")
+    .property_from_str("tune", "zerolatency")
+    .property_from_str("speed-preset", "ultrafast")
     .build()
-    .expect("Failed to create autovideosink");
+    .expect("Failed to create x264enc");
+
+  let payloader = ElementFactory::make("rtph264pay")
+    .name("rtp_payloader")
+    .build()
+    .expect("Failed to create rtph264pay");
+
+  let sink = ElementFactory::make("udpsink")
+    .name("udp_output")
+    .property("host", "127.0.0.1")
+    .property("port", 5000i32)
+    .build()
+    .expect("Failed to create udpsink");
 
   let pipeline = Pipeline::with_name("zerocast-capture-pipeline");
-  pipeline.add_many([&source, &videoconvert, &sink]).unwrap();
+  pipeline
+    .add_many([&source, &videoconvert, &encoder, &payloader, &sink])
+    .unwrap();
 
-  Element::link_many([&source, &videoconvert, &sink]).unwrap();
+  Element::link_many([&source, &videoconvert, &encoder, &payloader, &sink])
+    .unwrap();
 
-  println!("Starting screen capture... Look for a new window popping up!");
+  println!(
+    "Streaming started! Sending H.264 RTP packets to UDP 127.0.0.1:5000..."
+  );
   pipeline.set_state(State::Playing).unwrap();
 
   let bus = pipeline.bus().unwrap();
